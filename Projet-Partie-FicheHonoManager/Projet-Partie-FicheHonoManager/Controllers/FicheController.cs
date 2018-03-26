17fe2279e2bdf868b7637191a10ee4270d943836
KevinDupeyrat <k.dupeyrat@protonmail.com>
2018-03-26 15:06:33 +0200

    Initialisation

---
+++ Projet-Partie-FicheHonoManager/Projet-Partie-FicheHonoManager/Controllers/FicheController.cs
@@ -0,0 +1,40 @@
+﻿using Projet_Partie_FicheHonoManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projet_Partie_FicheHonoManager.Controllers
+{
+    public class FicheController : Controller
+    {
+
+        // Voir pour une injection de dépendance
+        private FicheHonoManager ficheHonoManager = new FicheHonoManager();
+
+        
+        public bool CreerFicher()
+        {
+            return ficheHonoManager.CreerFicher();
+        }
+
+
+        public List<FicheHono> ConsulterFiches()
+        {
+            return ficheHonoManager.ConsulterFiches();
+        }
+
+
+        public FicheHono ModifierFiche(FicheHono fiche)
+        {
+            return ficheHonoManager.ModifierFiche(fiche);
+        }
+
+
+        public FicheHono RechercheFiche(int id)
+        {
+            return ficheHonoManager.RechercheFiche(id);
+        }
+    }
+}
---
+++ Projet-Partie-FicheHonoManager/Projet-Partie-FicheHonoManager/Models/FicheHonoManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet_Partie_FicheHonoManager.Models
+{
+    public class FicheHonoManager
+    {
+
+        /*
+         * Méthode qui permet la création d'une fiche
+         * honoraire.
+         * Elle renvoie un booléen pour attester de
+         * la réussite de la création
+         * */
+        public bool CreerFicher()
+        {
+            return false;
+        }
+
+
+        /*
+         * Méthode qui permet la consultation
+         * de toutes les fiches de la base
+         * de données.
+         * */
+        public List<FicheHono> ConsulterFiches()
+        {
+            return null;
+        }
+
+
+        /*
+         * Méthode qui permet la modification
+         * d'une fiche honoraire et l'a
+         * renvoie une fois modifier.
+         * */
+        public FicheHono ModifierFiche(FicheHono fiche)
+        {
+            return null;
+        }
+
+
+        /*
+         * Méthode qui permet la recherche d'une ficher
+         * honoraire dans la base de données par son ID
+         * */
+        public FicheHono RechercheFiche(int id)
+        {
+            return null;
+        }
+    }
+}
